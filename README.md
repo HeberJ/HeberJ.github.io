1a998a7142f1fe114355ec4a90a81f88af5d699e
Heeb <iheberj@gmail.com>
2018-12-22 22:11:17 -0600

    replaced my portfolio with new react portfolio

--- README.md
+++
@@ -1,1 +0,0 @@
-# HeberJ.github.io
--- assets/javascript/script.js
+++
@@ -1,5 +0,0 @@
-//console.log(screen.height);
-//$('body').mousemove(function() {
-//    $('#intro-page').height($(window).height());
-//    $('#intro-page').width($(window).width());
-//});
